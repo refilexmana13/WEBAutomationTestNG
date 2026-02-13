407a5ab30fc6e1c163ff97fcd3c588059a77b160
Refi Lexmana <refi.lexmana13@gmail.com>
2026-02-14 00:34:26 +0700

    headless

--- src/test/java/com/Refi/BaseTest.java
+++ src/test/java/com/Refi/BaseTest.java
@@ -2,12 +2,17 @@ package com.Refi;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseTest {
     protected static WebDriver driver;
 
     protected void getDriver (){
-        // menggunakan Microsoft Edge sebagai browsernya
-        driver = WebDriverManager.chromedriver().create();
+        // menggunakan chrome sebagai browsernya
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(options);
     }
 }
